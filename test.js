5eb268922f3d72e79430d490352acc02b47561f9
Jacek <jacekard@gmail.com>
2020-03-10 18:01:09 +0100

    update questions

--- test.js
+++ test.js
@@ -1,19 +1,21 @@
 ﻿function losulosu() {
     var questions = [
         "1. Dlaczego włączamy flagę ip_forward (przekierowanie ruchu) podczas ataku MitM?",
-        "2. Do jakiego rodzaju monitorowania wykorzystywany jest program Wireshark?",
-        "3. Jakiego filtra użyjemy do wyświetlenia pakietów TCP na porcie 53 pochodzących z adresu 192.168.1.1?",
-        "4. Jakie filtra użyjemy do wyświetlenia pakietów HTTP kierowanych na adres 127.0.0.1?",
-        "5. Na czym polega sniffing pasywny?",
-        "6. Na czym polega sniffing aktywny?",
+        "2. Do jakiego rodzaju monitorowania wykorzystywany jest program NMAP?",
+        "3. Jakiego filtra użyjemy do wyświetlenia pakietów UDP na porcie 17 pochodzących z adresu 172.31.255.255?",
+        "4. Jakiego filtra użyjemy do wyświetlenia żądania HTTP kierowanych na adres 10.0.52.37?",
+        "5. Jakiego filtra użyjemy do wyświetlenia pakietów nie ARP i nie DNS o adresie MAC ff:ff:ff:ff:ff:ff?",
+        "6. Jakiego filtra użyjemy do wyświetlenia pakietów SMTP, które zawierają treść “Subject:”?",
         "7. Na czym polega atak typu arp-spoofing?",
-        "8. Zaleta podsłuchiwania (administrator)?",
         "9. Do czego służy narzędzie Wireshark?",
         "10. Do czego służy narzędzie PackETH?",
-        "11. Do czego służy narzędzie Netstat?",
-        "12. Do czego służy narzędzie TcpDump?",
-        "13. Do czego służy narzędzie Nmap?",
-        "14. Do czego służy narzędzie Arpspoof?"
+        "11. Do czego służy narzędzie Driftnet?",
+        "12. Czym różni się program Wireshark od TcpDump?",
+        "13. Na czym polega sniffing pasywny?",
+        "14. Na czym polega sniffing aktywny?",
+        "15. Do czego służy narzędzie Arpspoof?",
+        "16. Na czym polega atak typu Man-in-the-Middle?",
+        "17. Do czego może zostać wykorzystane podsłuchiwanie przez hakera?"
     ];
 
     var result = [];
